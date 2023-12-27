7abd4aa2ed850a34c6fc6f102217c0253b2ee8a7
Titanaboa596 <116961326+Titanaboa596@users.noreply.github.com>
2023-12-27 15:26:18 -0500

    Add files via upload

--- project.ino
+++ project.ino
@@ -1,78 +1,108 @@
 #include <AFMotor.h>
-#include <SoftwareSerial.h>   
-#include <Servo.h>              // Add library
-Servo name_servo;    
-Servo axle_servo; // New servo for the axle
-SoftwareSerial Serial1(A1, A3); 
+#include <SoftwareSerial.h>
+#include <Servo.h>  // Add library
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
-AF_DCMotor motor1(1); 
-AF_DCMotor motor2(2); 
+#define LEFT 0
+#define RIGHT 1
 
+LiquidCrystal_I2C lcd(0x20, 16, 2);
 
-int dist;                     
-int strength;                
-int check;                    
+Servo name_servo;
+SoftwareSerial Serial1(A1, A3);
+
+AF_DCMotor motor1(1);
+AF_DCMotor motor2(2);
+AF_DCMotor motor3(3);
+AF_DCMotor motor4(4);
+
+int dist;
+int strength;
+int check;
 int i;
-int uart[9];                   
-const int HEADER = 0x59;      
+int uart[9];
+const int HEADER = 0x59;
 
-void setup()
-{
+void setup() {
 
-  name_servo.attach (9);  
-  axle_servo.attach(10); // Attach the axle servo to slot 10
+  lcd.begin();
+  // Turn on the blacklight and print a message.
+  lcd.backlight();
 
-  Serial.begin(9600);         
-  Serial1.begin(115200);     
+  name_servo.attach(10);
 
-  motor1.setSpeed(200);    
-  motor2.setSpeed(200);    
-   
+  Serial.begin(9600);
+  Serial1.begin(115200);
 
+  motor1.setSpeed(200);
+  motor2.setSpeed(200);
+  motor3.setSpeed(250);
+  motor4.setSpeed(250);
 }
 
 void loop() {
 
- 
-
-  if (Serial1.available())               
-  {
-    if (Serial1.read() == HEADER)        
-    {
+  if (Serial1.available()) {
+    if (Serial1.read() == HEADER) {
       uart[0] = HEADER;
-      if (Serial1.read() == HEADER)      
-      {
+      if (Serial1.read() == HEADER) {
         uart[1] = HEADER;
-        for (i = 2; i < 9; i++)         
-        {
+        for (i = 2; i < 9; i++) {
           uart[i] = Serial1.read();
         }
         check = uart[0] + uart[1] + uart[2] + uart[3] + uart[4] + uart[5] + uart[6] + uart[7];
-        if (uart[8] == (check & 0xff))       
-          dist = uart[2] + uart[3] * 256;     
-          strength = uart[4] + uart[5] * 256; 
+        if (uart[8] == (check & 0xff))
+          dist = uart[2] + uart[3] * 256;
+        strength = uart[4] + uart[5] * 256;
 
-            Serial.println(dist);
+        Serial.println(dist);
+        lcd.clear();  // Clear the LCD screen
+        lcd.print("Distance: " + (dist));
 
-            if(dist < 10) {
-            
-            axle_servo.write(45); // Turn the axle servo slightly left
-          
-          }
+        if (dist < 5) {
+          // Your sensor code here
+          int leftDist;  // Get the distance on the left
+          int rightDist;  // Get the distance on the right
 
-          if(dist < 5) {
-            motor1.run(RELEASE); // Stop the motors
-            motor2.run(RELEASE);
-            
-            name_servo.write(90); // Turn the lidar servo 90 degrees left
+          if (leftDist > rightDist) {
+            turnLeft();
           } else {
-            motor1.run(FORWARD); // Move the motors forward
-            motor2.run(FORWARD);
-            name_servo.write(0); // Turn the lidar servo back to the center
-            axle_servo.write(0); // Turn the axle servo back to the center
+            turnRight();
           }
+        } else {
+          goForward();
         }
       }
     }
   }
+}
 
+void turnLeft() {
+  // Code to turn left
+  motor1.run(BACKWARD);
+  motor2.run(FORWARD);
+  motor3.run(FORWARD);
+  motor4.run(BACKWARD);
+  lcd.print("         Turning Left");
+  delay(1000);
+}
+
+void turnRight() {
+  // Code to turn right
+  motor1.run(FORWARD);
+  motor2.run(BACKWARD);
+  motor3.run(BACKWARD);
+  motor4.run(FORWARD);
+  lcd.print("         Turning Right");
+  delay(1000);
+}
+
+void goForward() {
+  // Code to go forward
+  motor1.run(FORWARD);
+  motor2.run(FORWARD);
+  motor3.run(FORWARD);
+  motor4.run(FORWARD);
+  lcd.print("        Going Forward");
+}
